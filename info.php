0a5c96c8d14bbc1ac82dfbd088049da473093caa
ebksei <alejandrof.platero@hotmail.com>
2018-04-21 17:44:52 +0200

    Información

---
+++ info.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+  <?php echo file_get_contents('rsc/head.html'); ?>
+
+  <title>Gimnasios SNK</title>
+</head>
+<body class="bg-externo">
+
+
+    <div class="sufee-login d-flex align-content-center flex-wrap">
+        <div class="container">
+            <div class="login-content">
+                <div class="login-logo" style="font-size:3em">
+                    <a href="index.html">
+                        Gimnasios SNK
+                    </a>
+                </div>
+                <div class="card">
+                  <div class="card-header">
+                    <h4>Dirección</h4>
+                  </div>
+                  <div class="card-body">
+                    Distrito TROST</br>
+                    Calle de la Piruleta, 11
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-header">
+                    <h4>Tarifas</h4>
+                  </div>
+                  <div class="card-body">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-5">MICHOZA</div>
+                        <div class="col-7">La perfecta para los soldados ahorradores.</div>
+                      </div>
+                      <div class="row">
+                        <div class="col-5">MICASA</div>
+                        <div class="col-7 text-justify">Para los soldados de élite de nuestras tropas.</div>
+                      </div>
+                      <div class="row">
+                        <div class="col-5">LEVI</div>
+                        <div class="col-7 text-justify">Si quieres llegar a estar tan en forma como un capitán, ésta es tu tarifa.</div>
+                      </div>
+                      <div class="row">
+                        <div class="col-5">TITÁNICA</div>
+                        <div class="col-7 text-justify">¿Quieres destrozar ciudadas con tan sólo tu puño? Con esta tarifa nadie se atreverá a toserte.</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-header">
+                    <h4>Zonas</h4>
+                  </div>
+                  <div class="card-body">
+                    <p class="text-justify"><b>ZONA DE AGUA:</b> Contamos con dos piscinas, una de ellas para los niños pequeños. Allí encontraréis los mejores instructores o, si lo preferís, podréis bañaros libremente.</p>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-6"></div>
+                        <div class="col-6"></div>
+                      </div>
+                    </div>
+                    <p class="text-justify"><b>ZONA DE FITNESS:</b> Donde lo darás todo por ponerte en forma.</p>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-6"></div>
+                        <div class="col-6 text-justify"><p>Con 10 apartados de función cardio-vascular y 10 máquinas para trabajar distintos múscilos. Además de la zona de peso libre. Contamos con una sala ciclo indoor con 15 bicicletas y equipo de sonido.</p></div>
+                      </div>
+                    </div>
+                    <p class="text-justify">Por último, pero no menos importante... !Dos salas para actividades dirigidas!</br></p>
+                    <p class="text-justify"><b>ZONA EXTERIOR:</b> Con...</p>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-6"></div>
+                        <div class="col-6 text-justify">3 pistas de pádel.</div>
+                      </div>
+                      <div class="row">
+                        <div class="col-6"></div>
+                        <div class="col-6 text-justify">3 pistas de tenis.</div>
+                      </div>
+                      <div class="row">
+                        <div class="col-6"></div>
+                        <div class="col-6 text-justify">1 campo de fútbol (césped).</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="assets/js/vendor/jquery-2.1.4.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+
+
+</body>
+</html>
